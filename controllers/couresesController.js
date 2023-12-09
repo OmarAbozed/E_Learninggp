e14e9e40b66f346e11d25b5d86aa0ee700c9e3d0
OmarAbozed <omarabozed58@email.com>
2023-12-09 22:10:46 +0200

    Add Controllers

---
+++ controllers/couresesController.js
@@ -0,0 +1,114 @@
+const asynchandler=require("express-async-handler");
+const{Courses,ValidationCreateCoures, ValidationUpdateCoures}=require("../models/Courses")
+
+/**
+ *  @desc    Get all Coureses
+ *  @route   /api/coureses
+ *  @method  GET
+ *  @access  public
+ */
+
+function getAllCoureses(){
+    asynchandler(async(req,res)=>{
+        const courses=await Courses.find(req.body);
+        res.status(200).json(courses)
+    })
+}
+
+/**
+ *  @desc    Get Coureses By ID
+ *  @route   /api/coureses/:id
+ *  @method  GET
+ *  @access  public
+ */
+
+function getCourseByID(){
+    asynchandler(async(req,res)=>{
+        const Course=await Courses.findById(req.params.id)
+        if(Course){
+            res.status(200).json(Course)
+        }else{
+            res.status(404).json({massage:"The Courese Not Found"})
+        }
+    })
+}
+
+/**
+ *  @desc    post  Coureses
+ *  @route   /api/coureses
+ *  @method  POST
+ *  @access  public  that by praivt after some time
+ */
+
+function postCourses(){
+    asynchandler(async(req,res)=>{
+        const { error }=ValidationCreateCoures(req.body);
+        if(error){
+            return res.status(400).json({massage:error.details[0].message})
+        }
+        const course=await new Courses({
+            title:req.body.title,
+            price:req.body.price,
+            field:req.body.field,
+            contint:req.body.contint
+        })
+    
+        const result =await course.save();
+        res.status(200).json(result)
+    })
+}
+
+/**
+ *  @desc    update  Coureses
+ *  @route   /api/coureses
+ *  @method  put
+ *  @access  public  that by praivt after some time
+ */
+
+function putCoureses(){
+    asynchandler(async(req,res)=>{
+        const { error }=ValidationUpdateCoures(req.body);
+        if(error){
+            return res.status(400).json({massage:error.details[0].massage})
+        }
+        const updatecourse=await Courses.findByIdAndUpdate(req.params.id,{
+            $set:{
+                title:req.body.title,
+                price:req.body.price,
+                field:req.body.field,
+                contint:req.body.contint
+            }
+        },{new:true})
+        res.status(200).json(updatecourse);
+    })
+}
+
+/**
+ *  @desc    delete  Coureses
+ *  @route   /api/coureses
+ *  @method  delete
+ *  @access  public  that by praivt after some time
+ */
+
+function deleteCourses(){
+    asynchandler(async(req,res)=>{
+        const course=await Courses.findById(req.params.id);
+        if(course){
+            await Courses.findByIdAndDelete(req.params.id)
+            res.status(200).json({massage:"Coures Is Deleted"})
+        }else{
+            res.status(404).json({massage:"Not Founded"})
+        }
+    
+    })
+}
+
+
+
+module.exports={
+    getAllCoureses,
+    getCourseByID,
+    postCourses,
+    putCoureses,
+    deleteCourses
+}
--- routes/course.js
+++ routes/course.js
@@ -1,104 +1,17 @@
 const express=require("express");
+const {getAllCoureses,getCourseByID,postCourses,putCoureses,deleteCourses}=require("../controllers/couresesController");
 const router=express.Router();
-const asynchandler=require("express-async-handler");
-const{Courses,ValidationCreateCoures, ValidationUpdateCoures}=require("../models/Courses")
 
+/*_______________API_______________*/
+router.get("/",getAllCoureses)
 
+router.get("/:id",getCourseByID)
 
-/**
- *  @desc    Get all Coureses
- *  @route   /api/coureses
- *  @method  GET
- *  @access  public
- */
+router.post("/",postCourses)
 
-router.get("/",asynchandler(async(req,res)=>{
-    const courses=await Courses.find(req.body);
-    res.status(200).json(courses)
-}))
+router.put("/:id",putCoureses)
 
-/**
- *  @desc    Get Coureses By ID
- *  @route   /api/coureses/:id
- *  @method  GET
- *  @access  public
- */
-
-router.get("/:id",asynchandler(async(req,res)=>{
-    const Course=await Courses.findById(req.params.id)
-    if(Course){
-        res.status(200).json(Course)
-    }else{
-        res.status(404).json({massage:"The Courese Not Found"})
-    }
-}))
-
-/**
- *  @desc    post  Coureses
- *  @route   /api/coureses
- *  @method  POST
- *  @access  public  that by praivt after some time
- */
-
-router.post("/",asynchandler(async(req,res)=>{
-    const { error }=ValidationCreateCoures(req.body);
-    if(error){
-        return res.status(400).json({massage:error.details[0].message})
-    }
-    const course=await new Courses({
-        title:req.body.title,
-        price:req.body.price,
-        field:req.body.field,
-        contint:req.body.contint
-    })
-
-    const result =await course.save();
-    res.status(200).json(result)
-}))
-
-
-/**
- *  @desc    update  Coureses
- *  @route   /api/coureses
- *  @method  put
- *  @access  public  that by praivt after some time
- */
-
-
-router.put("/:id",asynchandler(async(req,res)=>{
-    const { error }=ValidationUpdateCoures(req.body);
-    if(error){
-        return res.status(400).json({massage:error.details[0].massage})
-    }
-    const updatecourse=await Courses.findByIdAndUpdate(req.params.id,{
-        $set:{
-            title:req.body.title,
-            price:req.body.price,
-            field:req.body.field,
-            contint:req.body.contint
-        }
-    },{new:true})
-    res.status(200).json(updatecourse);
-}))
-
-/**
- *  @desc    delete  Coureses
- *  @route   /api/coureses
- *  @method  delete
- *  @access  public  that by praivt after some time
- */
-
-
-router.delete("/:id",asynchandler(async(req,res)=>{
-    const course=await Courses.findById(req.params.id);
-    if(course){
-        await Courses.findByIdAndDelete(req.params.id)
-        res.status(200).json({massage:"Coures Is Deleted"})
-    }else{
-        res.status(404).json({massage:"Not Founded"})
-    }
-
-}))
+router.delete("/:id",deleteCourses)
 
 //exports
 module.exports=router;
